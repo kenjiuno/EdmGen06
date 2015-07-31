f5c919da33b147a6b616c86577f85b627cc1990b
janbrogger <jan.brogger@holbergeeg.com>
2015-07-31 14:21:24 +0200

    Autogenerate MView45.cs when needed
    MView45.cs is needed but not present for the net45-ef6 build config.
    This commit autogenerates that file from MView40.cs and an awk script
    that was already present.

---
+++ Conv40To45.bat
@@ -0,0 +1,1 @@
+awk.exe "{gsub(\""System.Data.EntityClient\"",\""System.Data.Entity.Core.EntityClient\"")}  {gsub(\""System.Data.Objects\"",\""System.Data.Entity.Core.Objects\"")}  {gsub(\""System.Data.Metadata.Edm\"",\""System.Data.Entity.Core.Metadata.Edm\"")} {print}" EFv40\MView40.cs > MView45.cs
